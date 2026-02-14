bc4c78450d6e39626976b3e73f13093fb4dcc70d
gagerofficial <juhibeni0120@gmail.com>
2026-02-14 14:01:03 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -298,7 +298,7 @@ class DailyMenu(BaseModel):
 WEEKLY_MENU = [
     DailyMenu(
         day="Hétfő",
-        date="2025-02-14",
+        date="2025-02-16",
         soup=MenuItem(name="Legényfogóleves", allergens=["glutén", "tojás"]),
         main_course=MenuItem(name="Morzsás nudli porcukorral", allergens=["glutén", "tojás", "tej"]),
         dessert=MenuItem(name="Gyümölcs")
